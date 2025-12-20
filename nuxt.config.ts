94ac8795a910cac6642f01937904bef642138b5c
ahys <jiri.boudik1@gmail.com>
2025-12-20 22:22:02 +0100

    fav

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -27,6 +27,7 @@ export default defineNuxtConfig({
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
         { name: 'format-detection', content: 'telephone=no' },
+        { name: 'theme-color', content: '#4f46e5' },
         { name: 'author', content: 'Marek Cón' },
         { name: 'robots', content: 'index, follow' },
         { property: 'og:site_name', content: 'Fyzioterapie Marek Cón' },
@@ -34,6 +35,11 @@ export default defineNuxtConfig({
         { name: 'twitter:site', content: '@fyziomarcon' }
       ],
       link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+        { rel: 'manifest', href: '/site.webmanifest' },
         { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
         { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: 'anonymous' },
         { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap' },
